b09a6c3c6118a5b7c66ea33a94dce9eaed201efe
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-08-18 13:31:09 +0800

    Created a WebAssembly module we can use for testing

---
+++ crates/wasm/src/lib.rs
@@ -0,0 +1,48 @@
+use anyhow::Error;
+use wit_bindgen_rust::Handle;
+
+wit_bindgen_rust::export!("exports.wit");
+
+pub struct Exports;
+
+impl exports::Exports for Exports {
+    fn parse(
+        name: String,
+        contents: String,
+        wasm: Vec<u8>,
+    ) -> Result<Handle<Bindings>, exports::Error> {
+        let bindings = wit_pack::Bindings::from_src(&name, &contents, &wasm)?;
+        Ok(Handle::new(Bindings(bindings)))
+    }
+
+    fn parse_from_disk(
+        wit_file_path: String,
+        wasm_path: String,
+    ) -> Result<Handle<Bindings>, exports::Error> {
+        let bindings = wit_pack::Bindings::from_path(&wit_file_path, &wasm_path)?;
+        Ok(Handle::new(Bindings(bindings)))
+    }
+}
+
+pub struct Bindings(wit_pack::Bindings);
+
+impl exports::Bindings for Bindings {
+    fn generate_python(&self, _ouput_dir: String) -> Result<(), exports::Error> {
+        todo!();
+    }
+
+    fn generate_javascript(&self, output_dir: String) -> Result<(), exports::Error> {
+        self.0.javascript()?.save_to_disk(&output_dir)?;
+        Ok(())
+    }
+}
+
+impl From<Error> for exports::Error {
+    fn from(e: Error) -> Self {
+        exports::Error {
+            message: e.to_string(),
+            verbose: format!("{e:?}"),
+            causes: e.chain().map(|e| e.to_string()).collect(),
+        }
+    }
+}
