736bf106209b8baec2db089fb2f0cb20ef89f1e9
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-08-24 16:51:14 +0800

    Drop wapm-toml and accept everything via CLI flags

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -2,8 +2,9 @@ use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Error};
 use clap::Parser;
-use semver::{Version, VersionReq};
-use wapm_toml::{Manifest, Module};
+use wit_pack::{Abi, Interface, Metadata, Module};
+
+const POSSIBLE_ABIS: &[&str] = &["wasi", "none"];
 
 fn main() -> Result<(), Error> {
     let cmd = Cmd::parse();
@@ -17,7 +18,9 @@ fn main() -> Result<(), Error> {
 #[derive(Debug, Parser)]
 #[clap(version)]
 enum Cmd {
+    /// Generate bindings for use with NodeJS.
     Js(Codegen),
+    /// Generate Python bindings.
     Python(Codegen),
 }
 
@@ -26,33 +29,42 @@ struct Codegen {
     /// Where to save the generated bindings.
     #[clap(short, long)]
     out_dir: Option<PathBuf>,
-    /// The module to generate bindings for.
+    /// A `*.wit` file defining the exported interface.
     #[clap(short, long)]
-    module: Option<String>,
-    /// The path to check when looking for a WAPM package manifest.
-    #[clap(default_value = ".")]
-    path: PathBuf,
+    exports: PathBuf,
+    /// The name of the generated package.
+    #[clap(short = 'n', long = "name")]
+    package_name: String,
+    /// The generated package's version number.
+    #[clap(short = 'v', long = "version")]
+    package_version: String,
+    /// The `*.wasm` file bindings are being generated for.
+    #[clap(short, long)]
+    module: PathBuf,
+    #[clap(short, long, default_value = "none", parse(try_from_str), possible_values = POSSIBLE_ABIS)]
+    abi: Abi,
 }
 
 impl Codegen {
     fn run(self, language: Language) -> Result<(), Error> {
         let Codegen {
-            path,
             out_dir,
+            exports,
+            package_name,
+            package_version,
             module,
+            abi,
         } = self;
 
-        let manifest = Manifest::find_in_directory(&path).context("Unable to find wapm.toml")?;
-        let module = get_desired_module(&manifest, module.as_deref())?;
-
-        let bindings_field = module
-            .bindings
-            .as_ref()
-            .context("The module doesn't declare any bindings")?;
-
-        let metadata = derive_metadata(&manifest);
-        let interface = load_interface(bindings_field, &manifest.base_directory_path)?;
-        let module = load_module(module, &manifest.base_directory_path)?;
+        let metadata = Metadata::new(package_name, package_version);
+        let module = Module::from_path(&module, abi)
+            .with_context(|| format!("Unable to load \"{}\"", module.display()))?;
+        let interface = Interface::from_path(&exports).with_context(|| {
+            format!(
+                "Unable to parse an interface definition from \"{}\"",
+                exports.display()
+            )
+        })?;
 
         let files = match language {
             Language::JavaScript => wit_pack::generate_javascript(&metadata, &module, &interface)?,
@@ -68,86 +80,22 @@ impl Codegen {
     }
 }
 
-fn load_interface(
-    bindings: &wapm_toml::Bindings,
-    base_dir: &Path,
-) -> Result<wit_pack::Interface, Error> {
-    let wapm_toml::Bindings {
-        wit_exports,
-        wit_bindgen,
-    } = bindings;
-    ensure_compatible(wit_bindgen)?;
-
-    wit_pack::Interface::from_path(base_dir.join(wit_exports))
-}
-
-fn load_module(module: &Module, base_dir: &Path) -> Result<wit_pack::Module, Error> {
-    let Module {
-        name, source, abi, ..
-    } = module;
-    let path = base_dir.join(source);
-    let wasm =
-        std::fs::read(&path).with_context(|| format!("Unable to read \"{}\"", path.display()))?;
-
-    let abi = match abi {
-        wapm_toml::Abi::None => wit_pack::Abi::None,
-        wapm_toml::Abi::Wasi => wit_pack::Abi::Wasi,
-        other => anyhow::bail!("ABI not supported by wit-pack: {other:?}"),
-    };
-
-    Ok(wit_pack::Module {
-        name: name.clone(),
-        abi,
-        wasm,
-    })
-}
-
-fn derive_metadata(manifest: &Manifest) -> wit_pack::Metadata {
-    let wapm_toml::Package {
-        name,
-        version,
-        description,
-        ..
-    } = &manifest.package;
-
-    wit_pack::Metadata::new(name, version.to_string()).with_description(description)
-}
-
-fn ensure_compatible(wit_bindgen: &Version) -> Result<(), Error> {
-    let compatible_version: VersionReq = wit_pack::WIT_PARSER_VERSION
-        .parse()
-        .expect("Should always be valid");
-
-    anyhow::ensure!(
-        compatible_version.matches(wit_bindgen),
-        "wit-pack is not compatible with WIT format {wit_bindgen} (expected {compatible_version})",
-    );
-
-    Ok(())
-}
-
-fn get_desired_module<'m>(
-    manifest: &'m Manifest,
-    target_module: Option<&str>,
-) -> Result<&'m Module, Error> {
-    match target_module {
-        Some(target_module) => manifest
-            .module
-            .as_ref()
-            .into_iter()
-            .flatten()
-            .find(|m| m.name == *target_module)
-            .with_context(|| format!("The manifest doesn't define a \"{target_module}\" module")),
-        None => match manifest.module.as_deref() {
-            Some([m]) => Ok(m),
-            Some([]) | None => Err(Error::msg("The manifest doesn't include any modules")),
-            Some([..]) => Err(Error::msg("The manifest defines more than one module, please specify which module to use with the --module flag")),
-        },
-    }
-}
-
 #[derive(Debug, Copy, Clone)]
 enum Language {
     JavaScript,
     Python,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn abi_options_are_all_usable() {
+        for abi in POSSIBLE_ABIS {
+            assert!(Abi::from_str(abi).is_ok(), "{abi}");
+        }
+    }
 }
