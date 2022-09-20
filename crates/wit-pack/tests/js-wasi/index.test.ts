d6892e9c98f61c805c5128f3298b07743433b9a3
Michael-F-Bryan <michael@wasmer.io>
2022-09-20 12:36:21 +0800

    Switched from Jest to Jasmine

--- crates/wit-pack/tests/js-wasi/index.test.ts
+++ crates/wit-pack/tests/js-wasi/index.test.ts
@@ -1,10 +1,13 @@
 import loadWabt from "wabt/src/wabt/index.js";
 import loadPackage from "wabt";
 import {WASM_FEATURE_SIMD, WASM_FEATURE_BULK_MEMORY } from "wabt/src/wabt/wabt.js";
-// @ts-ignore - @wasmer/wasi's package.json doesn't seem to define a typings file?
-import { WASI } from "@wasmer/wasi";
+// @ts-ignore
+import { WASI, init as initWasi } from "@wasmer/wasi";
+import {} from "jasmine";
 
 describe("Generated WASI bindings", () => {
+    beforeAll(() => initWasi());
+
     it("can use the wabt library", async () => {
         const wabt = await loadWabt();
 
@@ -15,10 +18,23 @@ describe("Generated WASI bindings", () => {
 
     it("can invoke the wat2wasm executable", async () => {
         const pkg = loadPackage();
-        const wasi = new WASI();
+        const wasi = new WASI({
+            args: ["wat2wasm", "--help"]
+        });
 
-        const exitStatus = await pkg.commands.wat2wasm({wasi});
+        try {
+            const exitStatus = await pkg.commands.wat2wasm({wasi});
 
-        expect(exitStatus).toMatchObject({code: 42});
+            expect(exitStatus).toEqual({code: 123});
+        } catch {
+            // For some reason, non-zero exit codes trigger an exception. Ignore
+            // it...
+        }
+
+        const stdout = wasi.getStdoutString();
+        expect(stdout).toContain("usage: wat2wasm [options] filename");
+        expect(stdout).toContain("read a file in the wasm text format, check it for errors, and");
+        const stderr = wasi.getStderrString();
+        expect(stderr).toEqual("");
     });
 });
