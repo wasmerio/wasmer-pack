f51108fdd909093e083b7ff2ca5698d0134b855a
Ayush Jha <ayush@wasmer.io>
2023-03-26 21:28:57 +0545

    Add description to package.json

--- crates/wasmer-pack/src/js/mod.rs
+++ crates/wasmer-pack/src/js/mod.rs
@@ -266,12 +266,18 @@ fn generate_package_json(needs_wasi: bool, metadata: &Metadata) -> SourceFile {
         serde_json::json!({})
     };
 
+    let description = match &metadata.description {
+        Some(description) => description.clone(),
+        None => "".to_string(),
+    };
+
     let package_json = serde_json::json!({
         "name": metadata.package_name.javascript_package(),
         "version": &metadata.version,
         "main": format!("src/index.js"),
         "types": format!("src/index.d.ts"),
         "type": "commonjs",
+        "description": description,
         "dependencies": dependencies,
     });
 
